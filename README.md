fd54f97d7fec1e04599deb9262452e47c1b943bc
Martin Klepsch <martinklepsch@googlemail.com>
2017-03-16 00:36:23 +0700

    add Mobiledoc using code + instructions

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Install NPM packages: `yarn install`
+2. Build foreign libs vector: `boot repl -e "(do (compute-foreign-libs) (System/exit 0))"`
+3. Start the build: `boot dev`
--- src/cljs/weekly_log/app.cljs
+++ src/cljs/weekly_log/app.cljs
@@ -1,10 +1,32 @@
 (ns weekly-log.app
-    (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            ;; [cljsjs.mobiledoc-kit]
+            [mobiledoc-kit :as mdk]))
 
-(rum/defc label [text]
+(rum/defcs editor < (rum/local nil ::editor) {:did-mount (fn [s]
+                                                           (.render (mdk/Editor.) @(::editor s))
+                                                           s)}
+  [s text]
   [:div
-   [:h1 "A label"]
-   [:p text]])
+   {:ref #(reset! (::editor s) %)}
+   
+   ])
+
+(defn set-birthdate! [{:keys [day month year]}]
+  (.setItem (.-localStorage js/window) "birthdate" (js/Date. year (dec month) day)))
+
+(rum/defcs birthdate < (rum/local {} ::date)
+  [s]
+  [:div 
+   [:input {:type "input" :placeholder "day" :on-change #(swap! (::date s) assoc :day (.. % -target -value))}]
+   [:input {:type "input" :placeholder "month" :on-change #(swap! (::date s) assoc :month (.. % -target -value))}]
+   [:input {:type "input" :placeholder "year" :on-change #(swap! (::date s) assoc :year (.. % -target -value))}]
+   [:button {:on-click #(set-birthdate! @(::date s))} "Go"]])
+
+(rum/defc app []
+  [:div
+   (birthdate)
+   (editor)])
 
 (defn init []
-  (rum/mount (label) (. js/document (getElementById "container"))))
+  (rum/mount (app) (. js/document (getElementById "container"))))
