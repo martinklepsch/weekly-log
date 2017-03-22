d45a58623e64a76002f615a66059711527dec3be
Martin Klepsch <martinklepsch@googlemail.com>
2017-03-22 21:04:01 +0700

    clean up source

--- src/cljs/weekly_log/app.cljs
+++ src/cljs/weekly_log/app.cljs
@@ -1,6 +1,5 @@
 (ns weekly-log.app
   (:require [rum.core :as rum]
-            ;; [cljsjs.mobiledoc-kit]
             [mobiledoc-kit :as mdk]))
 
 (rum/defcs editor < (rum/local nil ::editor) {:did-mount (fn [s]
@@ -8,25 +7,7 @@
                                                            s)}
   [s text]
   [:div
-   {:ref #(reset! (::editor s) %)}
-   
-   ])
-
-(defn set-birthdate! [{:keys [day month year]}]
-  (.setItem (.-localStorage js/window) "birthdate" (js/Date. year (dec month) day)))
-
-(rum/defcs birthdate < (rum/local {} ::date)
-  [s]
-  [:div 
-   [:input {:type "input" :placeholder "day" :on-change #(swap! (::date s) assoc :day (.. % -target -value))}]
-   [:input {:type "input" :placeholder "month" :on-change #(swap! (::date s) assoc :month (.. % -target -value))}]
-   [:input {:type "input" :placeholder "year" :on-change #(swap! (::date s) assoc :year (.. % -target -value))}]
-   [:button {:on-click #(set-birthdate! @(::date s))} "Go"]])
-
-(rum/defc app []
-  [:div
-   (birthdate)
-   (editor)])
+   {:ref #(reset! (::editor s) %)}])
 
 (defn init []
-  (rum/mount (app) (. js/document (getElementById "container"))))
+  (rum/mount (editor) (. js/document (getElementById "container"))))
